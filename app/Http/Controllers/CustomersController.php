840908c8993ee5aad548767d2c866b2251544822
MPoul jr <mpouljr1995@gmail.com>
2022-08-27 07:55:33 +0530

    customer

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,11 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class CustomersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         $customer=DB::table('customers')
@@ -35,22 +31,13 @@ class CustomersController extends Controller
         return $customer;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
         $id=0;
@@ -83,46 +70,25 @@ class CustomersController extends Controller
         return $customer;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\customers  $customers
-     * @return \Illuminate\Http\Response
-     */
+   
     public function show(customers $customers)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\customers  $customers
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit(customers $customers)
     {
         
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\customers  $customers
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, customers $customers)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\customers  $customers
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy(customers $customers)
     {
         //
