d008e08862a6f98088b40d1574ce223b2f38790c
smsivanujan <smsivanujan@gmail.com>
2022-09-08 11:54:19 +0530

    PULL REQUEST

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,46 @@
 <!doctype html>
-<html lang="en" dir="ltr"> <!-- This "app.blade.php" master page is used for all the pages content present in "views/livewire" except "custom" and "switcher" pages -->
-	<head>
+<html lang="en" dir="ltr">
+<!-- This "app.blade.php" master page is used for all the pages content present in "views/livewire" except "custom" and "switcher" pages -->
 
-		<!-- META DATA -->
-		<meta charset="UTF-8">
-		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="description" content="gStudent MAnagement System">
-		<meta name="author" content="SOBIZTECH PVT LTD">
-		<meta name="keywords" content="Student MAnagement System, Software, Srilanka, admin dashboard, admin template, admin, bootstrap 5, laravel admin, laravel admin dashboard template, laravel ui template, laravel admin panel, admin panel, laravel admin dashboard, laravel template, admin ui dashboard">
+<head>
 
-        @include('layouts.components.styles')
+    <!-- META DATA -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="gStudent MAnagement System">
+    <meta name="author" content="SOBIZTECH PVT LTD">
+    <meta name="keywords" content="Student MAnagement System, Software, Srilanka, admin dashboard, admin template, admin, bootstrap 5, laravel admin, laravel admin dashboard template, laravel ui template, laravel admin panel, admin panel, laravel admin dashboard, laravel template, admin ui dashboard">
 
-    </head>
+    @include('layouts.components.styles')
 
-    <body class="ltr app sidebar-mini light-mode">
+</head>
 
-        <!-- GLOBAL-LOADER -->
-		<div id="global-loader">
-			<img src="{{asset('assets/images/loader.svg')}}" class="loader-img" alt="Loader">
-		</div>
-		<!-- /GLOBAL-LOADER -->
+<body class="ltr app sidebar-mini light-mode">
 
-        <!-- PAGE -->
-		<div class="page">
-			<div class="page-main">
+    <!-- GLOBAL-LOADER -->
+    <div id="global-loader">
+        <img src="{{asset('assets/images/loader.svg')}}" class="loader-img" alt="Loader">
+    </div>
+    <!-- /GLOBAL-LOADER -->
 
-                @include('layouts.components.app-header')
+    <!-- PAGE -->
+    <div class="page">
+        <div class="page-main">
 
-                @include('layouts.components.app-sidebar')
+            @include('layouts.components.app-header')
 
-                <!--app-content open-->
-                <div class="app-content main-content mt-0">
-                    <div class="side-app">
+            @include('layouts.components.app-sidebar')
 
-                        <!-- CONTAINER -->
-                        <div class="main-container container-fluid">
-                            {{-- session --}}
+            <!--app-content open-->
+            <div class="app-content main-content mt-0">
+                <div class="side-app">
+
+                    <!-- CONTAINER -->
+                    <div class="main-container container-fluid">
+                        {{-- session --}}
                         @if (\Session::has('success'))
-                        <div class="alert alert-success session-msg"
-                            style="width: 50%; margin:0 auto 15px auto; text-align:center;">
+                        <div class="alert alert-success session-msg" style="width: 50%; margin:0 auto 15px auto; text-align:center;">
                             <p>{{ \Session::get('success') }}</p>
                         </div>
 
@@ -50,11 +51,10 @@
                                 }, 5000);
                             });
                         </script>
-                    @endif
+                        @endif
 
-                    @if (\Session::has('error'))
-                        <div class="alert alert-danger session-msg"
-                            style="width: 50%; margin:0 auto 15px auto; text-align:center;">
+                        @if (\Session::has('error'))
+                        <div class="alert alert-danger session-msg" style="width: 50%; margin:0 auto 15px auto; text-align:center;">
                             <p>{{ \Session::get('error') }}</p>
                         </div>
 
@@ -65,28 +65,46 @@
                                 }, 5000);
                             });
                         </script>
-                    @endif
-                    {{-- session end --}}
+                        @endif
+                        {{-- session end --}}
 
-                            @yield('content')
+                        @yield('content')
 
-                        </div>
                     </div>
                 </div>
-                    <!-- CONTAINER CLOSED -->
-             </div>
-
-            @include('layouts.components.modal')
-
-            @yield('modal')
-
-            @include('layouts.components.footer')
-
+            </div>
+            <!-- CONTAINER CLOSED -->
         </div>
-        <!-- page -->
 
-        @include('layouts.components.scripts')
+        @include('layouts.components.modal')
 
-    </body>
+        @yield('modal')
+
+        @include('layouts.components.footer')
+
+    </div>
+    <!-- page -->
+
+    @include('layouts.components.scripts')
+
+    <!-- APEXCHART JS -->
+    <script src="{{asset('assets/js/apexcharts.js')}}"></script>
+
+    <!-- INTERNAL SELECT2 JS -->
+    <script src="{{asset('assets/plugins/select2/select2.full.min.js')}}"></script>
+
+    <!-- CHART-CIRCLE JS-->
+    <script src="{{asset('assets/plugins/circle-progress/circle-progress.min.js')}}"></script>
+
+    <!-- INTERNAL DATA-TABLES JS-->
+    <script src="{{asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/datatable/js/dataTables.bootstrap5.js')}}"></script>
+    <script src="{{asset('assets/plugins/datatable/dataTables.responsive.min.js')}}"></script>
+
+    <!-- INDEX JS -->
+    <script src="{{asset('assets/js/index1.js')}}"></script>
+    <script src="{{asset('assets/js/index.js')}}"></script>
+
+</body>
 
 </html>
--- resources/views/layouts/components/scripts.blade.php
+++ resources/views/layouts/components/scripts.blade.php
@@ -29,3 +29,14 @@
         <!-- DATA TABLE JS-->
         <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
         <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+        <script src="../assets/plugins/datatable/js/dataTables.buttons.min.js"></script>
+        <script src="../assets/plugins/datatable/js/buttons.bootstrap5.min.js"></script>
+        <script src="../assets/plugins/datatable/js/jszip.min.js"></script>
+        <script src="../assets/plugins/datatable/pdfmake/pdfmake.min.js"></script>
+        <script src="../assets/plugins/datatable/pdfmake/vfs_fonts.js"></script>
+        <script src="../assets/plugins/datatable/js/buttons.html5.min.js"></script>
+        <script src="../assets/plugins/datatable/js/buttons.print.min.js"></script>
+        <script src="../assets/plugins/datatable/js/buttons.colVis.min.js"></script>
+        <script src="../assets/plugins/datatable/dataTables.responsive.min.js"></script>
+        <script src="../assets/plugins/datatable/responsive.bootstrap5.min.js"></script>
+        <script src="../assets/js/table-data.js"></script>
